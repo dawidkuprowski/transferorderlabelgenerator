2aeece9992e83a9ecbce8a664caa9e28e4d28c3a
Dawid Kuprowski <dawidkuprowski@gmail.com>
2023-03-14 03:46:11 +0100

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 document.body.innerHTML += `
-    <h1>xd</h1>
+    <h1>xddd</h1>
     <div class="uk-card uk-card-default uk-card-large uk-width-1-2@m uk-position-absolute uk-position-center">
         <div class="uk-card-header">
             <div class="uk-grid-small uk-flex-middle" uk-grid>
